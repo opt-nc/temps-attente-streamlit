0bbc334ebfd9bb140709d97a30298ed1eeb81695
MalcolmBrt <bertainamalcolm@gmail.com>
2024-10-12 10:36:13 +1100

    Optimisation usage API (#37)

--- app.py
+++ app.py
@@ -1,19 +1,10 @@
 import streamlit as st
 from streamlit_autorefresh import st_autorefresh
-from utils import load_apikey, get_current_time, gauge
-import requests
+from utils import *
 
 st.set_page_config(page_title='Temps d\'attente en agence OPT-NC', layout = 'wide', page_icon = 'assets/images/favicon.jpg', initial_sidebar_state = 'auto')
 
-st_autorefresh(interval=2 * 60 * 1000, key="dataframerefresh")
-
-apikey = load_apikey()
-headers = {
-    "x-apikey": apikey,
-    "Content-Type": "application/json"
-}
-
-API_TEMPS_ATTENTE_BASE_URL = "https://api.opt.nc/temps-attente-agences/"
+st_autorefresh(interval=INTERVALLE_AUTOREFRESH * 1000, key="dataframerefresh")
 
 current_time = get_current_time()
 st.markdown(
@@ -23,48 +14,30 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-# requête pour récuperer les commune
-response_communes = requests.get(API_TEMPS_ATTENTE_BASE_URL+"communes", headers=headers)
+# Récupérer les communes (mise en cache)
+communes = fetch_communes()
 
-if response_communes.status_code == 200:
-    # liste des communes
-    communes = response_communes.json()
-    #liste communes en MAJ
-    communes_maj = [x.upper() for x in communes]
+if communes:
     # menu déroulant pour sélection de commune
-    selected_commune = st.sidebar.selectbox("Sélectionnez une commune :", communes_maj)
-    
-    # Requête pour récuperer agences avec la commune sélectionner
-    response_agences = requests.get(API_TEMPS_ATTENTE_BASE_URL+"agences", headers=headers, params={"commune": selected_commune})
+    selected_commune = st.sidebar.selectbox("Sélectionnez une commune :", communes)
 
-    if response_agences.status_code == 200:
-        # liste de tout les "bâtiments" OPT
-        agences = response_agences.json()
+    # Récupérer les agences pour la commune sélectionnée (mise en cache)
+    agences = fetch_agences(selected_commune)
 
-        agences_OPT = []
-        # récupération des agences OPT
-        for agence in agences :
-            if agence["type"]=="AGENCE" or agence["type"]=="ANNEXE" :
-                agences_OPT.append(agence)
-   
+    if agences:
         # Extraire le nom de l'agence 
-        designation_agences = [agence['designation'] for agence in agences_OPT]
-        
+        designation_agences = [agence["designation"] for agence in agences]
+
         # menu déroulant pour sélection agence
         selected_agence = st.sidebar.selectbox("Sélectionnez une agence :", designation_agences)
 
-        # affichage logos partenaires
-        st.sidebar.image("assets/images/logo_opt.png", width=250)
-        st.sidebar.image("assets/images/logo_unc.jpg", width=250)
-        
         # ajout temps d'attente en texte
-        for agence in agences_OPT :
+        for agence in agences:
             if agence["designation"]==selected_agence:
                 temps_attente_agence=(agence["realAvgWaitingTimeMs"])/1000
         
         gauge(round(temps_attente_agence/60))
-
-    else:
-        st.write(f"Erreur lors de la récupération des agences : {response_agences.status_code}")
-else:
-    st.write(f"Erreur lors de la récupération des communes : {response_communes.status_code}")
+        
+# affichage logos partenaires
+st.sidebar.image("assets/images/logo_opt.png", width=250)
+st.sidebar.image("assets/images/logo_unc.jpg", width=250)
